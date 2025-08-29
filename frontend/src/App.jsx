6f1786584b754f64bffad15afe74934bdde84966
Marwen Boussabat <boussabatmarwen9@gmail.com>
2025-08-29 16:57:36 +0100

    consume back apis in front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,50 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TodoForm from "../components/TodoForm";
-import "./App.css";
 import TodoList from "../components/TodoList";
+import "./App.css";
 
 const App = () => {
   const [array, updateArray] = useState([]);
 
-  const handleDelete = (id) => {
+  //fetch todos from backend
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/user")
+      .then((res) => {
+        updateArray(res.data.data.todos);
+        console.log(res);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleDelete = async (id) => {
+    const td=array.filter((el) => el.id === id)
+    console.log(td);
+    const res = await axios.delete(`http://localhost:4000/api/user/${td[0].title}`);
     updateArray(array.filter((el) => el.id !== id));
   };
-  const addwithProps = (title) => {
+  const addwithProps = async (title) => {
     if (!title) return;
-    updateArray([...array, { id: uuidv4(), title, completed: false }]);
+    try {
+      const newItem = { id: uuidv4(), title, completed: false };
+      const res = await axios.post(
+        "http://localhost:4000/api/user/create",
+        newItem
+      );
+
+      updateArray([...array, { id: uuidv4(), title, completed: false }]);
+    } catch (er) {
+      console.log(er.message);
+    }
   };
 
   return (
     <>
       <TodoForm onsubmit={addwithProps} />
       <h1 className="header">Todo List</h1>
-      <TodoList array={array} handleDelete={handleDelete}/>
+      <TodoList array={array} handleDelete={handleDelete} />
     </>
   );
 };
